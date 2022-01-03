e1a0ed53b5706e343208a9e2d404bb4977559b35
Christopher Kile <binksir@gmail.com>
2022-01-02 23:54:38 -0500

    redirect for form

--- assets/js/main.js
+++ assets/js/main.js
@@ -421,3 +421,26 @@
     }
 
 })();
+
+// formspree redirect
+
+var form = document.getElementById("form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.innerHTML = "Talk to you soons!";
+        form.reset()
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
